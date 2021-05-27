75a5669114567a2c7cde2520d4b5e0cc909e919c
Mikey Ward <mw@mikey.io>
2021-05-27 19:53:33 -0400

    Remove Int convenience autoclosure declarations

--- Sources/Convenience/Extensions/Int+Convenience.swift
+++ Sources/Convenience/Extensions/Int+Convenience.swift
@@ -2,12 +2,12 @@
 public extension Int {
 
     /// Generate an array of values produced by executing the provided closure `self` times.
-    func of<Value>(_ value: @autoclosure () throws -> Value) rethrows -> [Value] {
+    func of<Value>(_ value: () throws -> Value) rethrows -> [Value] {
         return try (1...self).map { _ in try value() }
     }
     
     /// Execute the provided closure `self` times.
-    func times(_ execute: @autoclosure () throws -> Void) rethrows {
+    func times(_ execute: () throws -> Void) rethrows {
         try (1...self).forEach { _ in try execute() }
     }
 }
