4074d8a366732f4bc9af8e99c31bdbc55c75b268
Mikey Ward <mw@mikey.io>
2021-08-31 15:44:12 -1000

    Remove argument label from Logger initializer

--- Sources/Convenience/Extensions/Logger+Convenience.swift
+++ Sources/Convenience/Extensions/Logger+Convenience.swift
@@ -2,7 +2,7 @@
 import OSLog
 
 public extension Logger {
-    init(category: String) {
+    init(_ category: String) {
         self.init(subsystem: "\(ProcessInfo().processName)[\(Bundle.main.bundleIdentifier ?? "unknown bundle")]",
                   category: category)
     }
