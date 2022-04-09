adaa0db91603156bda88caad6e03facae5211148
Mikey Ward <mw@mikey.io>
2022-04-09 12:23:01 -0400

    Churn Package.swift & scheme

--- Package.swift
+++ Package.swift
@@ -1,21 +1,24 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Convenience",
+    
     platforms: [
         .iOS(.v14),
         .macOS(.v11),
         .tvOS(.v14),
         .watchOS(.v7)
     ],
+    
     products: [
         .library(
             name: "Convenience",
             targets: ["Convenience"]),
     ],
+    
     targets: [
         .target(
             name: "Convenience",
