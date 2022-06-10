6a43c369c1c2436d7e6806357cf7124b0faa11f2
Mikey Ward <mw@mikey.io>
2022-06-10 09:43:54 -0400

    Make Int extension methods @autoclosure

--- Sources/Convenience/Extensions/Int+Convenience.swift
+++ Sources/Convenience/Extensions/Int+Convenience.swift
@@ -3,13 +3,13 @@ public extension Int {
 
     /// Generate an array of values produced by executing the provided closure `self` times.
     /// Example: `let mockData = 50.of { Model.random() }`
-    func of<Value>(_ value: () throws -> Value) rethrows -> [Value] {
+    func of<Value>(_ value: @autoclosure () throws -> Value) rethrows -> [Value] {
         return try (1...self).map { _ in try value() }
     }
     
     /// Execute the provided closure `self` times.
     /// Example: `10.times { runSelfTest() }`
-    func times(_ execute: () throws -> Void) rethrows {
+    func times(_ execute: @autoclosure () throws -> Void) rethrows {
         try (1...self).forEach { _ in try execute() }
     }
 }
